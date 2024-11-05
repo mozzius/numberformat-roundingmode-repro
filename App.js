d96cde9c83026181c3a56c4251d48027f18fcb0d
Samuel Newman <mozzius@protonmail.com>
2024-11-05 18:18:47 +0000

    number format repro

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "@formatjs/intl-locale/polyfill-force";
+import "@formatjs/intl-pluralrules/polyfill-force";
+import "@formatjs/intl-numberformat/polyfill-force";
+import "@formatjs/intl-pluralrules/locale-data/en";
+import "@formatjs/intl-numberformat/locale-data/en";
+
+import { Text, SafeAreaView, StyleSheet } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.paragraph}>
+        {"1050 -> "}
+        {Intl.NumberFormat("en-US", {
+          notation: "compact",
+          maximumFractionDigits: 1,
+          roundingMode: "trunc",
+        }).format(1050)}
+      </Text>
+      <Text style={styles.paragraph}>(should be 1k)</Text>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    backgroundColor: "white",
+    padding: 8,
+    gap: 16,
+  },
+  paragraph: {
+    fontSize: 18,
+    textAlign: "center",
   },
 });
